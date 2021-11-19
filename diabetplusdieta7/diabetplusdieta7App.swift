b6ce45cbfc6bfc975cc6b732b8127ff68178da4c
alexey <alexeyweb60@gmail.com>
2021-11-19 18:23:18 +0200

    Initial Commit

---
+++ diabetplusdieta7/diabetplusdieta7App.swift
@@ -0,0 +1,17 @@
+//
+//  diabetplusdieta7App.swift
+//  diabetplusdieta7
+//
+//  Created by alexey on 19.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct diabetplusdieta7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
